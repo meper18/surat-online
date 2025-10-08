779e41c6fc8c10c95572755efd05519a5a305d92
Surat Online Developer <andi.sumbayak2@gmail.com>
2025-10-09 00:22:21 +0700

    Fix 419 error: Add CSRF middleware and change session driver to file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            // Add any routes that should be excluded from CSRF verification
+        ]);
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
